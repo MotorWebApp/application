bf13944d7741cd5646f13d1b9cc1e1e4b39af8d0
vgerasim <vladimir.gerasimov@intel.com>
2016-05-29 18:57:36 +0300

    test2

--- views/pageHeader.html.php
+++ views/pageHeader.html.php
@@ -9,7 +9,7 @@
 				<span class="icon-bar"></span>
 			</button>
 			<a class="navbar-brand" href="#">
-				<img src="views/image/logo2.png" alt=""><span>TEst APPNAME.RU</span>
+				<img src="views/image/logo2.png" alt=""><span>TEst   APPNAME.RU</span>
 			</a>
 		 </div><!--Navbar header End-->
 		<nav class="collapse navbar-collapse navigation" id="bs-example-navbar-collapse-1" role="navigation">
