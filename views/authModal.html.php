0a45f2e37ea8b24ef7f130deaaf8978ec54243b1
vgerasim <vladimir@gerasimov-design.ru>
2016-09-16 03:19:22 +0300

    some fixes

--- views/authModal.html.php
+++ views/authModal.html.php
@@ -6,7 +6,7 @@
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				<h4 class="modal-title" id="myModalLabel">Авторизация в системе</h4>
 			</div>
-			<div class="modal-body">
+			<div class="text-center modal-body">
 				<!-- Nav tabs -->
 				<ul class="nav nav-tabs" role="tablist">
 					<li role="presentation" class="active"><a href="#auth" aria-controls="auth" role="tab" data-toggle="tab">Вход</a></li>
@@ -18,12 +18,16 @@
 					<div role="tabpanel" class="tab-pane fade in active text-center" id="auth">
 						<form>
 							<div class="form-group">
-								<label for="InputEmailIn">Email</label>
-								<input type="email" class="form-control" id="InputEmailIn" placeholder="Email">
+								<div class="col-xs-6">
+									<label for="InputEmailIn">Email</label>
+									<input type="email" class="form-control" id="InputEmailIn" placeholder="Email">
+								</div>
 							</div>
 							<div class="form-group">
-								<label for="InputPasswordIn">Пароль</label>
-								<input type="password" class="form-control" id="InputPasswordIn" placeholder="Пароль">
+								<div class="col-xs-6">
+									<label for="InputPasswordIn">Пароль</label>
+									<input type="password" class="form-control" id="InputPasswordIn" placeholder="Пароль">
+								</div>
 							</div>
 							<button type="submit" class="btn btn-default">Вход</button>
 						</form>
@@ -31,27 +35,33 @@
 					<div role="tabpanel" class="tab-pane fade text-center" id="reg">
 						<form>
 							<div class="form-group">
-								<label for="InputEmailUp">Email</label>
-								<input type="email" class="form-control" id="InputEmailUp" placeholder="Email">
+								<div class="col-xs-6">
+									<label for="InputEmailUp">Email</label>
+									<input type="email" class="form-control" id="InputEmailUp" placeholder="Email">
+								</div>
 							</div>
 							<div class="form-group">
-								<label for="InputPasswordUp1">Пароль</label>
-								<input type="password" class="form-control" id="InputPasswordUp1" placeholder="Пароль">
+								<div class="col-xs-6">
+									<label for="InputPasswordUp1">Пароль</label>
+									<input type="password" class="form-control" id="InputPasswordUp1" placeholder="Пароль">
+								</div>
 							</div>
 							<div class="form-group">
-								<label for="InputPasswordUp2">Пароль еще раз</label>
-								<input type="password" class="form-control" id="InputPasswordUp2" placeholder="Повтор пароля">
+								<div class="col-xs-6">
+									<label for="InputPasswordUp2">Пароль еще раз</label>
+									<input type="password" class="form-control" id="InputPasswordUp2" placeholder="Повтор пароля">
+								</div>
 							</div>
 							<button type="submit" class="btn btn-default">Регистрация</button>
 						</form>
 					</div>
 				</div>
-				<div style="width: 100%; height: 20px; border-bottom: 1px solid black; text-align: center">
-					<span style="font-size: 16px; background-color: #F3F5F6; padding: 0 10px;">
+				<div style="width: 100%; height: 20px; margin-bottom: 20px; padding: 20px 0; border-bottom: 1px solid black; text-align: center">
+					<span style="font-size: 16px; background-color: #fff; padding: 0 10px;">
 						или через VK <!--Padding is optional-->
 					</span>
 				</div>
-				<button type="button" class="btn btn-primary"><span class="social social-vk"></span>Войти через ВК</button>
+				<button type="button" class="btn btn-primary"><i class="fa fa-vk" aria-hidden="true"></i>Войти через ВК</button>
 			</div>
 			<div class="text-center modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">Отмена</button>
