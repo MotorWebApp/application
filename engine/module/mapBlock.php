6add014acd86b6b345eae3a58817433090b41c49
vgerasim <vladimir@gerasimov-design.ru>
2016-09-20 01:40:24 +0300

    one more fix

--- engine/module/mapBlock.php
+++ engine/module/mapBlock.php
@@ -8,7 +8,7 @@
 
 function mapBlock( $args ) {
 	$args = json_decode( $args );
-	return render( "mapBlock.html.php", null, $args );
+	return render( "mapBlock.html.php", null, array( "lon" => $args->lon, "lat" => $args->lat, "cityname" => $args->name ) );
 }
 
 ?>
